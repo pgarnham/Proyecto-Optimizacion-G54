499f9182d6a2f1bfebd0f8664d2f70b77c2b5a9f
Pablo Garnham <pgarnham@uc.cl>
2019-10-11 21:47:36 -0300

    arreglando problemas base-centro y ordenando en carpetas

--- conjuntos.py
+++ conjuntos.py
@@ -2,8 +2,8 @@
 
 # Bases Quinta Región -------------------------------------------
 
-bases = ["Centro_Regulador", "SAMU_Viña_del_Mar", "SAMU_Quintero",
-         "SAMU_Quillota", "SAMU_La_Ligua"]
+bases = ["Centro Regulador", "SAMU Viña del Mar", "SAMU Quintero",
+         "SAMU Quillota", "SAMU La Ligua"]
 
 # ---------------------------------------------------------------
 
@@ -15,21 +15,21 @@ ambulancias = [f"Ambulancia_{str(numero + 1)}" for numero in range(18)]
 
 # Centros Medicos -----------------------------------------------
 
-with open("centros_de_salud.csv", "r", encoding="utf-8") as file:
+with open("conjuntos/centros_de_salud.csv", "r", encoding="utf-8") as file:
     centros = [linea.split(";")[0] for linea in file if "\ufeffH" not in linea]
 
 # ---------------------------------------------------------------
 
 # Periodos de Tiempo (un día) -----------------------------------
 
-with open("periodos.csv", "r", encoding="utf-8") as file:
+with open("conjuntos/periodos.csv", "r", encoding="utf-8") as file:
     periodos = [line.split(";")[0] for line in file if "\ufeffT" not in line]
 
 # ---------------------------------------------------------------
 
 # Prestaciones --------------------------------------------------
 
-with open("prestaciones.txt", "r", encoding="utf-8") as file:
+with open("conjuntos/prestaciones.txt", "r", encoding="utf-8") as file:
     prestaciones = [line.strip() for line in file]
 
 # Parámetros cortos o constantes: -------------------------------
@@ -60,24 +60,24 @@ for tupla in aux_namig:
 
 # Ambulancias que hay en cada Base --------------------------------
 
-ambulancias_por_base = {centro: set() for centro in centros}
-ambulancias_por_base["Centro_Regulador"].add(ambulancias[0])
-ambulancias_por_base["SAMU_Viña_del_Mar"].add(ambulancias[1])
-ambulancias_por_base["SAMU_Viña_del_Mar"].add(ambulancias[2])
-ambulancias_por_base["SAMU_Viña_del_Mar"].add(ambulancias[3])
-ambulancias_por_base["SAMU_Viña_del_Mar"].add(ambulancias[4])
-ambulancias_por_base["SAMU_Viña_del_Mar"].add(ambulancias[5])
-ambulancias_por_base["SAMU_Quintero"].add(ambulancias[6])
-ambulancias_por_base["SAMU_Quintero"].add(ambulancias[7])
-ambulancias_por_base["SAMU_Quintero"].add(ambulancias[8])
-ambulancias_por_base["SAMU_Quintero"].add(ambulancias[9])
-ambulancias_por_base["SAMU_Quillota"].add(ambulancias[10])
-ambulancias_por_base["SAMU_Quillota"].add(ambulancias[11])
-ambulancias_por_base["SAMU_Quillota"].add(ambulancias[12])
-ambulancias_por_base["SAMU_Quillota"].add(ambulancias[13])
-ambulancias_por_base["SAMU_La_Ligua"].add(ambulancias[14])
-ambulancias_por_base["SAMU_La_Ligua"].add(ambulancias[15])
-ambulancias_por_base["SAMU_La_Ligua"].add(ambulancias[16])
-ambulancias_por_base["SAMU_La_Ligua"].add(ambulancias[17])
+ambulancias_por_base = {base: set() for base in bases}
+ambulancias_por_base["Centro Regulador"].add(ambulancias[0])
+ambulancias_por_base["SAMU Viña del Mar"].add(ambulancias[1])
+ambulancias_por_base["SAMU Viña del Mar"].add(ambulancias[2])
+ambulancias_por_base["SAMU Viña del Mar"].add(ambulancias[3])
+ambulancias_por_base["SAMU Viña del Mar"].add(ambulancias[4])
+ambulancias_por_base["SAMU Viña del Mar"].add(ambulancias[5])
+ambulancias_por_base["SAMU Quintero"].add(ambulancias[6])
+ambulancias_por_base["SAMU Quintero"].add(ambulancias[7])
+ambulancias_por_base["SAMU Quintero"].add(ambulancias[8])
+ambulancias_por_base["SAMU Quintero"].add(ambulancias[9])
+ambulancias_por_base["SAMU Quillota"].add(ambulancias[10])
+ambulancias_por_base["SAMU Quillota"].add(ambulancias[11])
+ambulancias_por_base["SAMU Quillota"].add(ambulancias[12])
+ambulancias_por_base["SAMU Quillota"].add(ambulancias[13])
+ambulancias_por_base["SAMU La Ligua"].add(ambulancias[14])
+ambulancias_por_base["SAMU La Ligua"].add(ambulancias[15])
+ambulancias_por_base["SAMU La Ligua"].add(ambulancias[16])
+ambulancias_por_base["SAMU La Ligua"].add(ambulancias[17])
 
 # -----------------------------------------------------------------
